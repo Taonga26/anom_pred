46cf8fae65751a85b329c81c346839f640bd60b6
Taonga26 <116643851+Taonga26@users.noreply.github.com>
2025-12-04 14:42:07 +0200

    Uncomment server run command in app.py

--- app.py
+++ app.py
@@ -197,5 +197,6 @@ def predict():
 # Run server
 # ------------------------------
 
-#if __name__ == '__main__':
-#    app.run(debug=True, port=8000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(port=8000, host='0.0.0.0')
+
